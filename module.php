45d787e704594023e8e9f4aa71de936733927ac5
Christian Hagedorn <hagis.iphone@googlemail.com>
2026-03-12 06:21:03 +0100

    Add files via upload

---
+++ module.php
@@ -0,0 +1,14 @@
+{
+  "id": "{A1B2C3D4-E5F6-7890-ABCD-EF0987654321}",
+  "name": "Task Manager",
+  "type": 3,
+  "vendor": "",
+  "aliases": [
+    "Task Manager"
+  ],
+  "url": "",
+  "parentRequirements": [],
+  "childRequirements": [],
+  "implemented": [],
+  "prefix": "TM"
+}
